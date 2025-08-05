080ffc0d7a6c8f14752c986ca1d6f7587b792ccb
sharvil-lade <sharvillade9917@gmail.com>
2025-08-05 19:38:14 +0530

    Added sign-up ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,17 +1,8 @@
 "use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import axios from "axios";
-import { useState } from "react";
+import React from "react";
 
-function page() {
-  const [username, setUsername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+const page = () => {
   return <div>page</div>;
-}
+};
 
 export default page;
