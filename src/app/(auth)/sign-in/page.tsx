345ae686784266281684f2e835137363e5adfdce
sharvil-lade <sharvillade9917@gmail.com>
2025-08-05 16:39:23 +0530

    Initialized shadcn for ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import axios from "axios";
+import { useState } from "react";
 
-const page = () => {
+function page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   return <div>page</div>;
-};
+}
 
 export default page;
