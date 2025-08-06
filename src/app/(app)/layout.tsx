8f57a5aa46b0a9b52970e3fdfd13a348d3959b45
sharvil-lade <sharvillade9917@gmail.com>
2025-08-06 13:33:19 +0530

    Refactored file structure and added starting files for dashboard page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import AuthProvider from "@/context/AuthProvoder";
 import { Toaster } from '@/components/ui/toaster';
 
