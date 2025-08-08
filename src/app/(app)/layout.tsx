f2dafd35b3a2ba5baea35223d770345e3ca36c7f
sharvil-lade <sharvillade9917@gmail.com>
2025-08-08 13:28:27 +0530

    Added main messaging page with some minor fixes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { Toaster } from '@/components/ui/toaster';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       {children}
+      <Toaster />
     </div>
   );
 }
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,9 +1,123 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { log } from "node:console";
 const page = () => {
-  return (
-    <div>Message page</div>
-  )
-}
+  const { username } = useParams() as { username: string };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      message: "",
+    },
+  });
+  function onSubmit(data: any) {
+    try {
+      axios
+        .post("/api/send-message", { username, content: data.message })
+        .then((response) => {
+          console.log(response.data);
+          alert("Message sent successfully");
+        })
+        .catch((error) => {
+          console.error("Error sending message:", error);
+          alert("Failed to send message");
+        });
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to send message");
+    }
+    setValue("message", "");
+  }
+  const [suggestedMessages, setSuggestedMessages] = useState<string[]>([]);
+  const [importNewMessages, setImportNewMessages] = useState(true);
+  useEffect(() => {
+    if (!importNewMessages) return;
 
-export default page
+    const fetchMessages = async () => {
+      try {
+        const response = await axios.get("/api/suggest-messages");
+        const messages = response.data.split("||");
+        setSuggestedMessages(messages);
+        console.log(messages);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      } finally {
+        setImportNewMessages(false);
+      }
+    };
+
+    fetchMessages();
+  }, [importNewMessages]);
+
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-20">
+        <h1 className="font-sans font-bold text-5xl flex">
+          Public profile page
+        </h1>
+      </div>
+      <div className="flex flex-col mt-10 mx-28 font-semibold text-2xl">
+        <div className="self-start mb-2">
+          <h2>Send Anonymous message to @{username}</h2>
+        </div>
+        <div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            <textarea
+              {...register("message", { required: true })}
+              className="border-2 border-gray-300 rounded-md p-2 w-full h-40 resize-none text-xl"
+              placeholder="Type your message here..."
+            />
+            {errors.message && (
+              <span className="text-red-500">This field is required</span>
+            )}
+            <Button type="submit" className="h-12 w-28 text-xl self-center">
+              Send
+            </Button>
+          </form>
+        </div>
+        <div className="mt-24">
+          <Button className="h-12  text-xl self-center">
+            <div
+              className="flex gap-2 font-normal text-lg"
+              onClick={() => setImportNewMessages(true)}
+            >
+              <span>Suggest messages</span>
+            </div>
+          </Button>
+        </div>
+        <div className="mt-8 font-normal">
+          <p>Click on any message below to see it</p>
+        </div>
+        <div className="border border-gray-300 p-6 rounded-lg mt-6 shadow-md bg-white mb-6">
+          <div className="flex flex-col gap-3 items-center">
+            {suggestedMessages.map((message, index) => (
+              <div
+                onClick={() => setValue("message", message)}
+                key={message}
+                className="bg-gray-100 p-4 w-full rounded-lg cursor-pointer transition-transform duration-200 ease-in-out hover:bg-gray-200 hover:scale-105 shadow-sm"
+              >
+                <p className="text-base text-gray-800 leading-relaxed">
+                  {message}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
